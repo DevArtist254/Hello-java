fd25003977055e1b9f8295b69fd2ece8be467809
DevArtist <kevinwasonga415@outlook.com>
2023-10-11 20:21:58 +0300

    Hello foo

--- src/SecClass.java
+++ src/SecClass.java
@@ -1,5 +1,6 @@
-public class FirstClass {
+public class SecClass {
     public static void main(String[] args) {
+        System.out.println("Hello, Lord");
         System.out.println("Hello, Kevin");
     }
 }
