79a87735cb641d3ed6205c065a74bdc52a47f005
DevArtist <kevinwasonga415@outlook.com>
2023-10-25 19:50:24 +0300

    Public Static Main Void Refactor

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -1,6 +1,7 @@
 public class MethodChallenge {
     public static void main(String[] args) {
         String player = "Tim";
+
         displayHighScorePosition(player,calHighScorePos(1500));
         displayHighScorePosition(player,calHighScorePos(1000));
         displayHighScorePosition(player,calHighScorePos(500));
@@ -13,9 +14,12 @@ public class MethodChallenge {
     }
 
     public static int calHighScorePos(int currentScore) {
-        if (currentScore >= 1000) return 1;
-        if (currentScore >= 500 && currentScore <= 1000) return 2;
-        if (currentScore >= 100 && currentScore <= 500) return 3;
-        return 4;
+        int position = 4;
+
+        if (currentScore >= 1000) position = 1;
+        if (currentScore >= 500) position = 2;
+        if (currentScore >= 100) position = 3;
+
+        return position;
     }
 }
